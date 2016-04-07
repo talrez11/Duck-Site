c1cdf39fa60e4aedc77cb40bd2712df2ad7ecadb
talrez11 <tal@way2cu.com>
2016-04-08 00:35:23 +0300

    Site: Added section#map

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -76,6 +76,50 @@ Site.on_load = function() {
 			recipe_container.removeClass('visible');
 		}
 	});
+
+	// Google maps
+	var locations = [
+	    ['ראשון לציון, ברשבסקי 33 א.ת. ישן', 31.9743357,34.8072087],
+	    ['אשדוד,קניון עד הלום', 31.7931702,34.6536022],
+	    ['קריית אליעזר,ת"א 11, חיפה', 32.8272847,34.9793037,17],
+	    ['נתניה זנגביל,זנגביל 13', 31.9557133,34.9998731,11],
+	    ['נתניה פולג,המלאכה 28 א.ת. פולג', 32.2790095,34.8393929,15],
+	    ['חדרה,דוד אליעזר 27', 32.4403779,34.9081889,17],
+	    ['נשר,הזית 1', 32.7734104,35.0366532,17],
+	    ['יוקנעם,הירדן 64', 32.6553668,35.0708915,14],
+	    ['חיפה,שבתאי לוי 21-23', 32.8143037,34.9912641,17],
+	    ['כרמיאל,הכבאי 1', 32.9230328,35.3027093,17],
+	    ['נהריה,סניף לוחמי הגטאות 9', 33.0010689,35.0948677,17],
+	    ['נצרת עלית,עמל 1', 32.7033148,35.3206897,17],
+	    ['קריית חיים,האיצטדיון 25', 32.8225287,35.0555778,17]
+	  ];
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 8,
+	center: new google.maps.LatLng(31.9743357,34.8072087),
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+	var infowindow = new google.maps.InfoWindow();
+	var base_url = window.location.origin;
+	var custom_icon = base_url + "/site/images/map-pin.png";
+	var marker, i;
+
+	for (i = 0; i < locations.length; i++) {  
+		marker = new google.maps.Marker({
+		position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+		map: map,
+		icon:custom_icon
+	});
+
+	google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		return function() {
+		infowindow.setContent(locations[i][0]);
+		infowindow.open(map, marker);
+	  }
+	})(marker, i));
+	}
+
 };
 
 
