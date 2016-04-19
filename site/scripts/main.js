c12ba245db7fe3196986ba2be07add80b22f9f1d
talrez11 <tal@way2cu.com>
2016-04-19 22:17:58 +0300

    Site: Fixed site structure and style

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -100,7 +100,7 @@ Site.on_load = function() {
 		}
 	});
 
-	if($('section#map').length > 0) {
+	if($('div#map').length > 0) {
 	// Google maps
 		var locations = [
 		    ['ראשון לציון, ברשבסקי 33 א.ת. ישן', 31.9743357,34.8072087],
